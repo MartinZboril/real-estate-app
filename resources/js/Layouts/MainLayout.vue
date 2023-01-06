7a683d891127b7a6438eec942e001766b5edbf99
webBook <martinzboril1@hotmail.com>
2023-01-06 10:13:31 +0100

    add CRUD for Listing

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <Link :href="route('listing.index')">Listings</Link>&nbsp;
+  <Link :href="route('listing.create')">New Listings</Link>
+  <div v-if="flashSuccess" class="success">
+    {{ flashSuccess }}
+  </div>
+  <slot />
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { Link, usePage } from '@inertiajs/inertia-vue3'
+
+const page = usePage()
+const flashSuccess = computed(
+  () => page.props.value.flash.success,
+)
+</script>
+
+<style scoped>
+.success {
+  background-color: green;
+  color: white;
+}
+</style>
